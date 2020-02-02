50b1c7d17de33aa0e52da657fa04479a398775f5
Arfarafar <45181367+Arfarafar@users.noreply.github.com>
2020-02-03 01:06:06 +0300

    Update cpu.c

--- language/cpu/cpu.c
+++ language/cpu/cpu.c
@@ -1,13 +1,25 @@
 #include <stdio.h>
-
+#include <cmath>
 #include <enum.h>
 #include <stdlib.h>
 #include <Stack.h>
 
 int Create_buf(FILE* flin, char** buf);
 
-int main() {
-    FILE *flin = fopen("../../../code/code.asmer", "rb");
+
+#include <types.h>
+int main(int argc, const char* argv[]) {
+
+    char input[MAXLEN] = "../../../code/code.asmer";
+    char output[MAXLEN] = "";
+
+    if (argv[1]) {
+        strcpy (input, argv[1]);
+        Getnames(input, output, "");
+    }
+    printf (">>>> %s: input \"%s\", output \"%s\"\n", argv[0], input, output);
+
+    FILE *flin = fopen(input, "rb");
     if (flin == nullptr) {
         printf("cannot open code.asmer \n");
         abort();
@@ -21,14 +33,16 @@ int main() {
     char* buf = nullptr;
     int length = Create_buf(flin, &buf);
 
-    stek_t *stk1 = Stack_Construct(4, "stek");
-    stek_t *stk_calls = Stack_Construct(4, "calls");
+    stek_t *stk1 = Stack_Construct (4, "stek");
+    stek_t *stk_calls = Stack_Construct (4, "calls");
 
     if (!stk1 || !stk_calls) {
-        printf("No possible to make this stack, memory error");
+        printf ("No possible to make this stack, memory error");
     }
 
-    int Registers[4] = {};
+
+    int Operative[MAXVARS*100] = {};
+    int Registers[5] = {};
     int PC = 0;
     char command = 0;
     int arg = 0;
@@ -37,21 +51,40 @@ int main() {
     case Num: code;  break;
 
 
-    while (PC < length) {
-        char command = *(buf + PC++);
+    while (PC < length && PC >= 0) {
+        command = *(buf + PC++);
+        //printf("before %d %d\n", PC++, command);
         switch (command) {
 #include <commands.h>
         }
-       // printf("\n%d\n", Registers[0]);
-        // Stack_Dump(stk1);
-       // printf("\n%d\n", Registers[0]);
-    }
+        /*printf("after %d %d\n", PC, command);
+        for(int i = 4; i < 15 ; i++){
+            printf("%d: %d ", i, Operative[i]);
+        }
 
+        printf("\n0r: %d ", Registers[0]);
+        printf("1r: %d ", Registers[1]);
+        printf("3r: %d ", Registers[3]);
+        printf("4r: %d\n", Registers[4]);
+        printf("\n"); */
+
+     //    Stack_Dump(stk1);
+        //printf("\n%d\n", Registers[4]);
+    }
+   // printf("\n%d\n", Registers[0]);
+ //  printf("\n%d\n", Registers[1]);
+    if (PC < 0)
+        printf("Неправильный прыжок команда %d", command);
+   // printf(" %d %d", PC, command);
     fclose(flin);
     return 0;
 }
 
+
 int Create_buf(FILE* flin, char** buf){
+    assert(flin);
+    assert(buf);
+
     fseek(flin, 0, SEEK_END);
     int length = ftell(flin) - (SIGNATURE_SIZE + VERSION_SIZE);
     fseek(flin, SIGNATURE_SIZE + VERSION_SIZE, SEEK_SET);

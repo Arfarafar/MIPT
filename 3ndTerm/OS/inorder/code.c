ebc3f7902c737b7af6c13ec1fe04f0d1b7227604
Andrew <andrew2001@bk.ru>
2020-10-07 18:11:21 +0300

    2nd prog

---
+++ 3ndTerm/OS/inorder/code.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <errno.h>
+
+
+
+
+struct M{
+	long type;
+};
+
+
+int main(int argc, char* argv[]){
+	if (argc != 2){
+		printf("Invalid argc\n");
+	}
+
+	char* extstr;
+	long chnum = strtol(argv[1], &extstr, 0);
+
+	struct M msg = {1};
+	
+
+	int msgid = msgget(IPC_PRIVATE, IPC_CREAT | 0666);
+
+	for(long i = 1; i < chnum + 1; i++){
+
+		pid_t ret_code = fork();
+
+		if (ret_code == 0){
+
+			if (!(msgrcv(msgid, &msg, 0, i, 0) + 1)){
+				perror("rcv");
+				return 1;
+			}
+
+			printf("m_pid %d; p_ppid %d; number %ld \n", getpid(), getppid(), i);
+			msg.type = i + 1;
+			msgsnd(msgid, &msg , 0, 0);
+			return 0;
+
+		}
+		
+	}
+
+	printf(" Я закончил\n");
+	msgsnd(msgid, &msg , 0, 0);
+	
+
+
+	while(wait(NULL)+1) {}
+
+	msgctl(msgid, IPC_RMID, NULL);
+	return 0;
+}

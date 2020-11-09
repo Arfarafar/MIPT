6f3a02470f752f34b5c05466cef0c0ff1453decd
Andrew <andrew2001@bk.ru>
2020-11-09 18:53:47 +0300

    signals

---
+++ 3ndTerm/OS/signals/signals.c
@@ -0,0 +1,132 @@
+#include <signal.h>
+#include <stdio.h>
+ #include <sys/types.h>
+  #include <unistd.h>
+#include <stdlib.h>
+ #include <sys/prctl.h>
+
+
+
+
+#define BITSIZEofCHAR 8
+#define firstbitMASK 0b10000000
+
+char bit = 0;
+
+
+void child(int sig){
+	exit(0);
+}
+
+
+void receiver(int sig){
+	if (sig == SIGUSR1){
+		bit = 1;
+
+	}
+	else
+		bit = 0;
+}
+
+void translator(int sig){
+
+}
+
+
+int main(int argc, char* argv[]){
+	if (argc > 2 || argc == 1){
+		printf("Invalid argc\n");
+		return 0;
+	}
+
+	sigset_t  blockingMask;
+	sigemptyset(&blockingMask);
+	sigaddset(&blockingMask, SIGUSR1);
+	sigaddset(&blockingMask, SIGUSR2);
+	sigprocmask(SIG_BLOCK, &blockingMask, NULL);
+
+	struct sigaction act = {};
+
+	act.sa_handler = child;
+	sigaction(SIGCHLD, &act, NULL);
+
+	prctl(PR_SET_PDEATHSIG, SIGCHLD);
+
+	pid_t parent_id = 1;
+	pid_t child_id = fork();
+
+	if (child_id == 0){
+
+		act.sa_handler = translator;
+		sigaction(SIGUSR1, &act, NULL);
+
+		act.sa_handler = child;
+		sigaction(SIGCHLD, &act, NULL);
+
+		if((parent_id = getppid()) == 1)
+			exit(1);
+		
+		FILE* flin = fopen(argv[1], "rb");
+		
+		sigdelset(&blockingMask, SIGUSR1);
+		
+
+		unsigned char buffer ;
+
+		do {
+			unsigned char buffer = EOF;
+			fread(&buffer, 1, 1, flin);
+			unsigned char tmp = buffer;
+
+
+			for (int i = 0; i < BITSIZEofCHAR; ++i)
+			{
+				sigsuspend(&blockingMask);
+				if (tmp & firstbitMASK){
+					kill(parent_id, SIGUSR1);
+				}
+				else{
+					kill(parent_id, SIGUSR2);
+				} 
+				
+				tmp <<= 1;
+			
+			}
+			
+		} while(buffer != EOF);
+
+
+	}
+
+	else{
+		
+		act.sa_handler = receiver;
+		sigaction(SIGUSR1, &act, NULL);
+		sigaction(SIGUSR2, &act, NULL);
+
+		sigdelset(&blockingMask, SIGUSR1);
+		sigdelset(&blockingMask, SIGUSR2);
+		
+		kill(child_id, SIGUSR1);
+
+		while(1){
+			char c = 0;
+
+			for (int i = 0; i < BITSIZEofCHAR; ++i)
+			{	
+				sigsuspend(&blockingMask);
+				c = (c << 1);
+				c |= bit;
+				
+				kill(child_id, SIGUSR1);
+			}
+
+			if(c == EOF)
+				break;
+			
+			write(STDOUT_FILENO, &c , 1);
+				
+		}
+
+	}
+}
